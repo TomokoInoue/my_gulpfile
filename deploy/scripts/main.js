e72667863ad2318376073196a2148b6d66dc4bb8
TomokoInoue <tomo.inoue0521@gmail.com>
2017-04-06 19:30:19 +0900

    [modify] scss内の記述を削除

--- deploy/scripts/main.js
+++ deploy/scripts/main.js
@@ -1,2 +1,2 @@
-!function(n,o){o.addEventListener("DOMContentLoaded",function(){var o=n.Sub,t=new o;t.show()},!1)}(window,window.document),function(n,o){function t(){}t.prototype={show:function(){console.log("こんちには")}},n.Sub=t}(window,window.document);
+!function(n,o){o.addEventListener("DOMContentLoaded",function(){var o=n.Sub,d=new o;d.show()},!1)}(window,window.document);
 //# sourceMappingURL=../_maps/main.js.map
